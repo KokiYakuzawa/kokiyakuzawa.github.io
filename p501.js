e0bd09183829d3b2a47e96aad9490ea124f62a9c
KokiYakuzawa <80326743+KokiYakuzawa@users.noreply.github.com>
2022-05-03 14:21:51 +0900

    Create p501.js

---
+++ p501.js
@@ -0,0 +1,16 @@
+// main.ts
+
+/// <reference path="p5.global-mode.d.ts" />
+
+function setup(): void {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw(): void {
+  background(0);
+
+  if (mouseIsPressed) {
+    fill(255);
+    ellipse(mouseX, mouseY, 50, 50);
+  }
+}
