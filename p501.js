b0f0ada06da99df095d61a0cf8d968f7871f0004
KokiYakuzawa <80326743+KokiYakuzawa@users.noreply.github.com>
2022-05-03 15:10:24 +0900

    Update p501.js

--- p501.js
+++ p501.js
@@ -3,6 +3,8 @@
 /// <reference path="p5.global-mode.d.ts" />
 
 function setup(){
+   window.addEventListener("touchstart", function (event) { event.preventDefault(); }, { passive: false });
+  window.addEventListener("touchmove", function (event) { event.preventDefault(); }, { passive: false });
   createCanvas(windowWidth, windowHeight);
 }
 
