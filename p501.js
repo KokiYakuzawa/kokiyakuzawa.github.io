3400c0cc8ecd0b0b6bc4fd7c4e247fbc57cf47f6
KokiYakuzawa <80326743+KokiYakuzawa@users.noreply.github.com>
2022-05-03 14:31:46 +0900

    Update p501.js

--- p501.js
+++ p501.js
@@ -2,13 +2,13 @@
 
 /// <reference path="p5.global-mode.d.ts" />
 
-function setup(): void {
+function setup(){
   createCanvas(windowWidth, windowHeight);
 }
 
-function draw(): void {
+function draw() {
   background(0);
-  ellipse(200,200,10,10);
+  ellipse(200,200,100,100);
 
   if (mouseIsPressed) {
     fill(255);
