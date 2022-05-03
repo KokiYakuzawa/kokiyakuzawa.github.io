b5b1891a2aa617a2e3624583cfa05c9915b47d63
KokiYakuzawa <80326743+KokiYakuzawa@users.noreply.github.com>
2022-05-03 15:38:42 +0900

    Update p501.js

--- p501.js
+++ p501.js
@@ -2,18 +2,35 @@
 
 /// <reference path="p5.global-mode.d.ts" />
 
-function setup(){
-   window.addEventListener("touchstart", function (event) { event.preventDefault(); }, { passive: false });
+
+
+ var i =0;
+function setup() {
+  createCanvas(windowWidth,windowHeight);
+ rectMode(CENTER);
+   
+   //画面を固定する記述
+    window.addEventListener("touchstart", function (event) { event.preventDefault(); }, { passive: false });
   window.addEventListener("touchmove", function (event) { event.preventDefault(); }, { passive: false });
   createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
-  background(0);
-  ellipse(200,200,100,100);
-
-  if (mouseIsPressed) {
-    fill(255);
-    ellipse(mouseX, mouseY, 50, 50);
+  
+  background(255);
+  noStroke();
+  fill(0);
+   i++;
+    ellipse(windowWidth/2,i,20,20)
+  if(i>windowHeight){
+    i=0;
   }
+  
+  fill(80);
+  rect(windowWidth/2,windowHeight/2,50,182);
+  fill(255);
+  rect(windowWidth/2,windowHeight/2,100,180);
+ 
+  
+  
 }
