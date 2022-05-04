22d923e95090c18967acc09925ea1790859e7283
KokiYakuzawa <80326743+KokiYakuzawa@users.noreply.github.com>
2022-05-04 23:03:39 +0900

    Update p501.js

--- p501.js
+++ p501.js
@@ -16,12 +16,12 @@ function preload() {
 
 
 function setup() {
-  wx= windowWidth;
-   wy= windowHeight;
+  wx= width;
+   wy= height;
 frameRate(30);
   background(255);
   createCanvas(wx,wy);
-  pg = createGraphics(windowWidth, windowHeight);
+  pg = createGraphics(width, height);
   img= loadImage("kaso.png"); 
  
   //画面を固定する記述
