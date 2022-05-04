5bbb3827172b8e71b26e429df872b1dde0bba223
KokiYakuzawa <80326743+KokiYakuzawa@users.noreply.github.com>
2022-05-04 19:07:32 +0900

    Update p501.js

--- p501.js
+++ p501.js
@@ -1,35 +1,32 @@
-// main.ts
-
-/// <reference path="p5.global-mode.d.ts" />
 
 
-
-
- 
 
 let img; //画像表示の変数
 let pg;
+let wx =windowWidth;
+let wy= windowHeight;
 
 //事前読み込みをpreload関数で行う
-function preload() {
+//function preload() {
   //変数を使って画像をロード
   /* img = loadImage("https://loremflickr.com/600/600"); */
-}
+//}
 
 //層を追加して線の軌跡を残しつつカーソルの軌跡を消すための何か
 
 
 function setup() {
-
+frameRate(30);
   background(255);
-  createCanvas(windowWidth,windowHeight);
-  pg = createGraphics(1920, 1080);
+  createCanvas(wx,wy);
+  pg = createGraphics(1000, 1080);
   /* img= loadImage("kaso.png"); */
  
-   //画面を固定する記述
+  //画面を固定する記述
     window.addEventListener("touchstart", function (event) { event.preventDefault(); }, { passive: false });
   window.addEventListener("touchmove", function (event) { event.preventDefault(); }, { passive: false });
   createCanvas(windowWidth, windowHeight);
+
 }
 
 function draw() {
@@ -38,39 +35,49 @@ function draw() {
   // map()で、マウスを動かした時のスケールを拡張。ここもう少しまとめられる気がする
   var mx = map(
     mouseX, 
-    400,800,
-    0, 10000
+    wx/2-200, wx/2+200, 
+    wx/2-5000, wx/2+5000
     );
   var my = map(
     mouseY, 
-    400,800,
-    0, 10000
+   wy/2-200, wy/2+200, 
+    wy/2-5000, wy/2+5000
     );
   var pmx = map(
     pmouseX, 
-    400,800,
-    0, 10000
+   wx/2-200, wx/2+200, 
+    wx/2-5000, wx/2+5000
     );
  var pmy = map(
     pmouseY, 
-   400,800,
-    0, 10000
+    wy/2-200, wy/2+200, 
+    wy/2-5000, wy/2+5000
     );
-    
+
  /* pg.beginDraw();*/
 //マウスを押したとき線を描く
   if (mouseIsPressed) {
-    pg.strokeWeight(18);
+   pg.strokeWeight(18);
+    
     pg.line(pmx, pmy, mx, my);
-   // pg.endDraw();
+  
    
   }
-   
-  background(255);
   
-rect(windowWidth/3, windowHeight/3, windowWidth*2/3,windowHeight*2/3);
+  
+  
+  background(255);
+   rect(wx/2-100, wy/2-60, 200, 120);
+  image(pg,0,0);
+
+
  /* image(pg, 0, 0);*/
 //デカいマウスを動かす
  /* image(img, mx-60, my-60); */
-   rect( mx-60, my-60,100,100);
+ rect( mx-60, my-60,100,100);
+ 
+ 
+  
+  
+      
 }
